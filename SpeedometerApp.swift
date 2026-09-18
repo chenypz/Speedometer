22637191649512f75c535f765165510fdfedecf0
chenypz <himjkill@gmail.com>
2026-09-19 03:41:43 +0800

    Update SpeedometerApp.swift

--- SpeedometerApp.swift
+++ SpeedometerApp.swift
@@ -2,9 +2,15 @@ import SwiftUI
 
 @main
 struct SpeedometerApp: App {
+    init() {
+        // 在 App 啟動時即刻接管 window，隱藏狀態列並讓畫面滿版
+        UIApplication.shared.isStatusBarHidden = true
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .persistentSystemOverlays(.hidden) // iOS 16+ 隱藏底部 Home Indicator 橫條 (達到手遊級沉浸感)
         }
     }
 }
